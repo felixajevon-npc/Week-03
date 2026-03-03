39937b810357107cf074c583d9e25eda59a05644
jevonkagenou <128831198+jevonkagenou@users.noreply.github.com>
2026-03-03 14:50:26 +0700

    Menambahkan logika penentu bilangan prima

--- src/main.dart
+++ src/main.dart
@@ -1,4 +1,25 @@
 void main() {
   print('--- Pengecek Bilangan Prima ---');
   List<int> angkaUji = [2, 4, 7, 10, 13];
+
+  for (int angka in angkaUji) {
+    bool isPrima = true;
+
+    if (angka <= 1) {
+      isPrima = false;
+    } else {
+      for (int i = 2; i <= angka ~/ 2; i++) {
+        if (angka % i == 0) {
+          isPrima = false;
+          break;
+        }
+      }
+    }
+
+    if (isPrima) {
+      print('$angka adalah Bilangan Prima');
+    } else {
+      print('$angka BUKAN Bilangan Prima');
+    }
+  }
 }
